a200d42435411adf6ccfc71f23c1e8885b49881a
amaljom <amaljom99@gmail.com>
2022-07-26 19:08:08 +0200

    build your dream section completed

--- src/components/BuildYourTeam.vue
+++ src/components/BuildYourTeam.vue
@@ -7,12 +7,53 @@
         <h1 class="font-w-h1">BUIL YOUR DREAM&nbsp;</h1>
         <h1>TODAY</h1>
     </div>
+    <!-- inserisco i 4 component dinamicamente -->
+    <div class="d-flex justify-content-around my-5">
+        <div class="px-3" v-for="(element, index) in elements" :key="index">
+            <img :src="require(`../assets/${element.img}`)" alt="">
+            <h5>{{ element.name }}</h5>
+            <p> {{element.info}} </p>
+            <div class="star-now-button">
+                <a @click.prevent :href="`${element.url}`">Start now&nbsp;&nbsp;&nbsp;<i class="fa-solid fa-arrow-right-long"></i></a>
+            </div>
+            
+        </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-
+    data:function(){
+        return {
+            elements:[
+                {
+                    img:'home-5-image-box-01.png',
+                    name: 'Idea Discussione',
+                    info: 'Get teamed up with the specialists ow work and teach coding for years at famous universities.',
+                    url: '#'
+                },
+                {
+                    img:'home-5-image-box-02.png',
+                    name: 'Web Development',
+                    info: 'Learn to start building a webpage from scratch. You decide your own pace, course and speed.',
+                    url: '#'
+                },
+                {
+                    img:'home-5-image-box-03.png',
+                    name: 'System Administration',
+                    info: 'Learners are encouraged to study tge mechanism and structure of system administration.',
+                    url: '#'
+                },
+                {
+                    img:'home-5-image-box-04.png',
+                    name: 'Graphic Design',
+                    info: 'Have a passion for graphics and arts? Show your talents with confidence and self-assertiveness',
+                    url: '#'
+                }
+            ]
+        }
+    }
 }
 </script>
 
@@ -30,5 +71,24 @@ export default {
 h1{
     font-size: 3rem;
     color: #20adad;
+}
+h5{
+    font-weight: bolder;
+}
+.star-now-button{
+    padding: 10px;
+    background-color: #ffffff;
+    margin: 0 auto;
+    widows: 80%;
+    transition-duration: .3s;
+    border-radius: 15px;
+    &:hover{
+        background-color: #f8f8f8;
+    }
+}
+.star-now-button a{
+    text-decoration: none;
+    color: #8e8ba4;
+    font-weight: bolder;
 }
 </style>
