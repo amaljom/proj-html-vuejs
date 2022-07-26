a8dcdf2064be89b683b82839f4c40e9baa306cc9
amaljom <amaljom99@gmail.com>
2022-07-26 16:32:39 +0200

    header section completed

--- src/components/ContainerWithImg.vue
+++ src/components/ContainerWithImg.vue
@@ -1,5 +1,21 @@
 <template>
-  
+  <div class="d-flex justify-content-between mt-5 py-3 background-container">
+    <div class="container-size">
+        <!-- titolo -->
+        <h1>Complete Your Dreams In Javas Programming</h1>
+        <!-- scritta -->
+        <p>
+            Learn any coding program in distance and for a reasonable fee.
+            You don't gabe to struggle alone, you've got out assitance and help.
+        </p>
+        <!-- Bottone -->
+        <div class="d-flex text-white button">
+            <i class="fa-solid fa-download pe-3"></i>
+            <span>Download Free E-book</span>
+        </div>
+    </div>
+    <div> <img src="../assets/home-5-hero-image.png" alt=""> </div>
+  </div>
 </template>
 
 <script>
@@ -8,6 +24,33 @@ export default {
 }
 </script>
 
-<style>
+<style scoped lang="scss">
+img{
+    width: 550px;
+}
+h1{
+    color: #3f3a64;
+    font-size: 4rem;
+    font-weight: bolder;
+}
+p{
+   color: #b4b1be;
+   font-weight: bolder;
+   font-size: 20px;
+}
+.container-size{
+    width: 35%;
+}
+.button{
+    padding: 18px 19px;
+    background-color: #20ad96;
+    border-radius: 12px;
+    width: 52%;
+    transition: .2s;
+     &:hover{
+        background-color: #3f3a64;
+        cursor: pointer;
+     }
+}
 
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,31 +1,34 @@
 <template>
   <header>
-    <div class="nav d-flex justify-content-between align-items-center">
-        <div> <img class="w-50" src="../assets/dark-logo.png" alt=""> </div>
-        
-        <div class="d-flex align-items-center">
-            <!-- lista -->
-            <div>
-                <ul class="m-0">
-                    <li v-for="(element, index) in navBarElements" :key="index"> {{ element.item}} <i class="fa-solid fa-angle-down font-icon"></i> </li>
-                </ul>
-            </div>
-            <!-- icons -->
-            <div >
-                <span class="px-2"> <i class="fa-solid fa-cart-shopping"></i> </span>
-                <span class="px-2"> <i class="fa-regular fa-user p-1"></i> </span>
-            </div>
-            <!-- seard -->
-            <div>
-                <div class="input-dec">
-                    <input type="text" name="" placeholder="Search...">
-                    <i class="fa-solid fa-magnifying-glass text-secondary"></i>
+        <div class="nav div-position">
+            <div class="nav d-flex justify-content-between align-items-center">
+                <div> <img class="w-50" src="../assets/dark-logo.png" alt=""> </div>
+                
+                <div class="d-flex align-items-center">
+                    <!-- lista -->
+                    <div>
+                        <ul class="m-0">
+                            <li v-for="(element, index) in navBarElements" :key="index"> {{ element.item}} <i class="fa-solid fa-angle-down font-icon"></i> </li>
+                        </ul>
+                    </div>
+                    <!-- icons -->
+                    <div >
+                        <span class="px-2"> <i class="fa-solid fa-cart-shopping"></i> </span>
+                        <span class="px-2"> <i class="fa-regular fa-user p-1"></i> </span>
+                    </div>
+                    <!-- seard -->
+                    <div>
+                        <div class="input-dec">
+                            <input type="text" name="" placeholder="Search...">
+                            <i class="fa-solid fa-magnifying-glass text-secondary"></i>
+                        </div>
+                    </div>
                 </div>
             </div>
+            <!-- Creazione del component contente la grossa immagine -->
+            <ContainerWithImg />
         </div>
-    </div>
-    <!-- Creazione del component contente la grossa immagine -->
-    <ContainerWithImg />
+        <div class="img-container"><img class="img-backgorund" src="../assets/jumbo-overlay.svg" alt="Non Trovato"></div>
   </header>
 </template>
 
@@ -67,7 +70,18 @@ export default {
 <style lang="scss" scoped>
 header{
     background-color: #e9e6e3;
-    height: 500px;
+}
+.img-container{
+    width: 100%;
+    position: relative;
+}
+.img-backgorund{
+    width: 100%;
+    height: 400px;
+    -webkit-transform: scaleY(-1);
+    transform: scaleY(-1);
+    position: absolute;
+    bottom: 0;
 }
 .input-dec{
     padding: 12px 10px;
@@ -85,11 +99,16 @@ ul li{
     cursor: pointer;
     display: inline;
     padding: 25px ;
+    color: #3f3a64;
 }
 .nav{
     width: 1250px;
     margin: 0 auto;
     padding-top: 10px;
+}
+.div-position{
+    position: relative;
+    z-index: 99;
 }
 .font-icon{
     font-size:10px;
