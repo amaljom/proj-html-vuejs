48c3a535a05792505da8823e16301eb0168485a7
amaljom <amaljom99@gmail.com>
2022-07-26 14:01:58 +0200

    installed bootstrap via install e and fontawsome via cdn in main

--- src/App.vue
+++ src/App.vue
@@ -20,12 +20,5 @@ export default {
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "~bootstrap/scss/bootstrap.scss";
 </style>
