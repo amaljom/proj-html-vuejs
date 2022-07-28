627fb4f1d40f57397ef94be309fdbffbb919a0f7
amaljom <amaljom99@gmail.com>
2022-07-28 14:07:34 +0200

    added a basic carousel function

--- src/components/main/TeamIntro.vue
+++ src/components/main/TeamIntro.vue
@@ -8,8 +8,8 @@
                     <h1>what make they&nbsp;<span class="thinner">love us?</span></h1>
                 </div>
             </div>
-            <div class="pt-5 mt-5 row justify-content-around ">
-                <div v-for="(person, index) in people" :key="index" class="text-center card-member">
+            <div v-if="n===0" class="pt-5 mt-5 row justify-content-around ">
+                <div v-for="(person, index) in people.first" :key="index" class="text-center card-member">
                     
                     <div class="profile-pic"> <img :src="require(`../../assets/img/${person.img}`)" alt=""> </div>
                     
@@ -22,7 +22,43 @@
                     <span> {{ person.profession}} </span>
                 </div>
             </div>
-            
+            <div v-else-if="n===1" class="pt-5 mt-5 row justify-content-around ">
+                <div v-for="(person, index) in people.second" :key="index" class="text-center card-member">
+                    
+                    <div class="profile-pic"> <img :src="require(`../../assets/img/${person.img}`)" alt=""> </div>
+                    
+                    <p class="info">
+                        {{person.info}}
+                    </p>
+                    <p class="fw-bold">
+                        {{ person.name }}
+                    </p>
+                    <span> {{ person.profession}} </span>
+                </div>
+            </div>
+            <div v-else-if="n===2" class="pt-5 mt-5 row justify-content-around ">
+                <div v-for="(person, index) in people.third" :key="index" class="text-center card-member">
+                    
+                    <div class="profile-pic"> <img :src="require(`../../assets/img/${person.img}`)" alt=""> </div>
+                    
+                    <p class="info">
+                        {{person.info}}
+                    </p>
+                    <p class="fw-bold">
+                        {{ person.name }}
+                    </p>
+                    <span> {{ person.profession}} </span>
+                </div>
+            </div>
+
+            <div class="d-flex justify-content-around">
+                <div @click="prevTree()" class="btn btn-danger">
+                    <i class="fa-solid fa-arrow-left"></i>
+                </div>
+                <div @click="nextTree()" class="btn btn-danger">
+                    <i class="fa-solid fa-arrow-right"></i>
+                </div>
+            </div>
         </div>
     </div>
 </template>
@@ -31,27 +67,81 @@
 export default {
     data:function(){
         return{
-            people:[
-                {
-                    img:'testimonial-avata-02.jpg',
-                    info:'I am free to learn at my own pace, follow my own schedule and choose the subject I like. Great study portal for people like me.',
-                    name:'MINO HOLLACE',
-                    profession:'/ Freelancer'
-                },
-                {
-                    img:'testimonial-avata-01.jpg',
-                    info:'MaxCoach is my best choice. Their tutors are smart and professional when dealing with students.',
-                    name:'MADLEY PONDOR',
-                    profession:'/ IT Specialst'
-                },
-                {
-                    img:'testimonial-avata-04.jpg',
-                    info:'I am happy with their arrangerment of lessons and subject. They reflect a scientific investigation.',
-                    name:'LUVIC DUBBLE',
-                    profession:'/ Private Tutor'
-                }
+            people:{
+                first:[
+                    {
+                        img:'testimonial-avata-02.jpg',
+                        info:'I am free to learn at my own pace, follow my own schedule and choose the subject I like. Great study portal for people like me.',
+                        name:'MINO HOLLACE',
+                        profession:'/ Freelancer'
+                    },
+                    {
+                        img:'testimonial-avata-01.jpg',
+                        info:'MaxCoach is my best choice. Their tutors are smart and professional when dealing with students.',
+                        name:'MADLEY PONDOR',
+                        profession:'/ IT Specialst'
+                    },
+                    {
+                        img:'testimonial-avata-04.jpg',
+                        info:'I am happy with their arrangerment of lessons and subject. They reflect a scientific investigation.',
+                        name:'LUVIC DUBBLE',
+                        profession:'/ Private Tutor'
+                    }
+                ],
+                second:[
+                    
+                    {
+                        img:'testimonial-avata-01.jpg',
+                        info:'MaxCoach is my best choice. Their tutors are smart and professional when dealing with students.',
+                        name:'MADLEY PONDOR',
+                        profession:'/ IT Specialst'
+                    },
+                    {
+                        img:'testimonial-avata-04.jpg',
+                        info:'I am happy with their arrangerment of lessons and subject. They reflect a scientific investigation.',
+                        name:'LUVIC DUBBLE',
+                        profession:'/ Private Tutor'
+                    },
+                    {
+                        img:'testimonial-avata-02.jpg',
+                        info:'I am free to learn at my own pace, follow my own schedule and choose the subject I like. Great study portal for people like me.',
+                        name:'MINO HOLLACE',
+                        profession:'/ Freelancer'
+                    },
+                ],
+                third:[
+                    
+                    {
+                        img:'testimonial-avata-01.jpg',
+                        info:'MaxCoach is my best choice. Their tutors are smart and professional when dealing with students.',
+                        name:'MADLEY PONDOR',
+                        profession:'/ IT Specialst'
+                    },
+                    {
+                        img:'testimonial-avata-02.jpg',
+                        info:'I am free to learn at my own pace, follow my own schedule and choose the subject I like. Great study portal for people like me.',
+                        name:'MINO HOLLACE',
+                        profession:'/ Freelancer'
+                    },
+                    {
+                        img:'testimonial-avata-04.jpg',
+                        info:'I am happy with their arrangerment of lessons and subject. They reflect a scientific investigation.',
+                        name:'LUVIC DUBBLE',
+                        profession:'/ Private Tutor'
+                    },
+                    
+                ]
                 
-            ],
+            },
+            n:0
+        }
+    },
+    methods:{
+        nextTree(){
+            this.n++;
+        },
+        prevTree(){
+            this.n--;
         }
     }
 }
